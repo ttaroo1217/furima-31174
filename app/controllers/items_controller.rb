70708b6e01bd6196d0c7dd40b4356b89d8372483
ttaroo1217 <69450720+ttaroo1217@users.noreply.github.com>
2020-11-04 18:47:49 +0900

    itemモデル単体テスト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.create(item_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
@@ -23,7 +23,6 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :status_id, :delivery_charge_id, :area_id, :days_id, :price, :image).merge(user_id: current_user.id)
-    # params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,30 +1,30 @@
 class Item < ApplicationRecord
-  with_options presence: true do
-    validates :name
-    validates :description
-    validates :user
-    validates :category_id
-    validates :status_id
-    validates :delivery_charge_id
-    validates :area_id
-    validates :days_id
-    validates :price
-  end
-
-  validates :category_id, numericality: { other_than: 1 }
-  validates :status_id, numericality: { other_than: 1 }
-  validates :delivery_charge_id, numericality: { other_than: 1 }
-  validates :area_id, numericality: { other_than: 1 }
-  validates :days_id, numericality: { other_than: 1 }
-        
+  extend ActiveHash::Associations::ActiveRecordExtensions
   has_one :item_order
   belongs_to :user
   has_one_attached :image
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
+  
   belongs_to :category
   belongs_to :status
   belongs_to :delivery_charge
   belongs_to :area
-  belongs_to :day
+  belongs_to :day  
+
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :image
+    validates :price
+  end
+
+  validates :price, numericality: true
+  validates :price, inclusion: { in: (300..9999999)}
+
+  validates :category_id, numericality: { other_than: 0 }
+  validates :status_id, numericality: { other_than: 0 }
+  validates :delivery_charge_id, numericality: { other_than: 0 }
+  validates :area_id, numericality: { other_than: 0 }
+  validates :days_id, numericality: { other_than: 0 }
 end
